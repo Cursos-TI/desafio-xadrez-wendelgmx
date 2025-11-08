84ae590f1edfe4fca6ed43c12cbbd605d0b72f10
Wendel <109918190+wendelgmx@users.noreply.github.com>
2025-11-08 17:28:09 +0000

    Novato

--- xadrez.c
+++ xadrez.c
@@ -17,6 +17,42 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
+    const char cima[25] = "Uma casa acima";
+    const char baixo[25] = "Uma casa abaixo";
+    const char direita[25] = "Uma casa à direita";
+    const char esquerda [25] = "Uma casa à esquerda";
+
+   //Torre
+   printf("Movimento Torre\n");
+   int iTorre = 1;
+   while(iTorre <=5){
+      printf("%d - %s", iTorre, direita);
+      printf("\n");
+      iTorre++;
+   }
+
+   printf("########################\n");
+
+   //Bispo
+   printf("Movimento Bispo\n");
+   for (int iBispo = 1; iBispo <= 5; iBispo++){
+     
+      printf("%d - %s\t%s", iBispo, cima, direita);
+      printf("\n");
+      
+   }
+   
+   printf("########################\n");
+
+   //Rainha
+   printf("Movimento Rainha\n");
+   int nRainha = 1;
+   do{
+      printf("%d - %s", nRainha, esquerda);
+      printf("\n");
+      nRainha++;
+   }while (nRainha <=8);
+
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
